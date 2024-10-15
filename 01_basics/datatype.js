bf3bc811847c52c723c7f38c57924ddb3257f365
dikshapanwar <138571165+dikshapanwar@users.noreply.github.com>
2024-10-15 07:28:09 +0000

    DataType

---
+++ 01_basics/datatype.js
@@ -0,0 +1,13 @@
+//1. PREMITIVE DATA TYPE
+
+let name= "Diskha"//String=>""
+let age=21//Number
+let isLoggedIn=false//Boolean =>true  ? False
+let state;//undefined =>
+//bigint =>when number is big
+let stauts= null//null=>standalone Value
+//symbol =>unique
+
+//2.OBJECT
+console.log(typeof null)//Object
+console.log(typeof undefined )//undefined
