5b871e9a36610122dbc719d308b6bf72c16f9095
dikshapanwar <138571165+dikshapanwar@users.noreply.github.com>
2024-10-15 11:18:55 +0000

    object merge

--- 02_basics/object.js
+++ 02_basics/object.js
@@ -38,5 +38,48 @@ jsUser.greetingTwo =function()
     console.log(`Hello Js ${this.name} ,How Are you`);
     
 }
-console.log(jsUser.greeting());
-console.log(jsUser.greetingTwo());
+// console.log(jsUser.greeting());
+// console.log(jsUser.greetingTwo());
+
+
+
+//++++++++++++++++++++++++++++***********************02 Object Basics***********************************+++++++++++++++++++++++++++++++++++
+//  const tinderUser= new Object();
+const tinderUser ={}
+tinderUser.id="123ABCUY";
+tinderUser.name="USer";
+tinderUser.email="user@gmail.com"
+tinderUser.isLoggedIn=false
+//  console.log(tinderUser);
+
+
+const regularUser={
+    email:"diksha@gmail.com",
+    fullname:{
+userfullname:{
+    firstname:"Diksha",
+    lastname:"Panwar"
+}
+    }
+}
+console.log(regularUser.fullname.userfullname.lastname);
+
+ 
+//Merge object
+
+const obj1={
+    1:"a",
+    2:"b",
+    3:"c",
+    4:"d"
+}
+const obj2={
+    4:"e",
+    5:"f",
+    6:"g",
+    7:"h"
+}
+
+// const obj3={...obj1,...obj2};
+const obj3=Object.assign({},obj1,obj2)
+console.log(obj3);
