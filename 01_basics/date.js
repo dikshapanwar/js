819771b3b1a3a4db05f04e4c2b69f6af520836ff
dikshapanwar <138571165+dikshapanwar@users.noreply.github.com>
2024-10-15 09:08:45 +0000

    Date And TIme

--- 01_basics/date.js
+++ 01_basics/date.js
@@ -1,3 +1,29 @@
-let myDate =new Date();
-console.log(myDate.toString());
-console.log(myDate.to)
+let myDate =new Date();//=>OBJECT
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+//console.log(typeof myDate)
+
+
+// let myCreatedDate= new Date(2023,0,23);
+// let myCreatedDate= new Date(2023,0,23,5,3);
+//let myCreatedDate= new Date("2024-04-27");//yyyy-mm-dd
+let myCreatedDate= new Date("04-27-2003");
+//console.log(myCreatedDate.toLocaleString());
+
+//TIMESTAMP
+
+let myTimeStamp= Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());log
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate= new Date();
+// console.log(newDate.getMonth())
+// console.log(newDate.getDay())
+
+console.log(newDate.toLocaleString('default',{
+    weekday:"long"
+}));
