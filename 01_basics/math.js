7405f896faeed4b4bbe6d39c541d98d5fd134e4c
dikshapanwar <138571165+dikshapanwar@users.noreply.github.com>
2024-10-15 07:25:13 +0000

    Number And Math

---
+++ 01_basics/math.js
@@ -0,0 +1,37 @@
+const score =200;
+// console.log(score);
+const balance=new Number(100);
+// console.log(balance);
+//  console.log(balance.toString());
+//  console.log(typeof balance);
+
+ //console.log(balance.toFixed(2));
+
+
+ const otherNumber=123.988;
+ //console.log(otherNumber.toPrecision(3));
+
+ const hundreds=1000000;
+//  console.log(hundreds.toLocaleString('en-IN'))
+
+
+ //***********************************************************************MATHS******************************************************************
+// console.log(Math);
+//console.log(Math.abs(-5));
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.2));
+// // console.log(Math.floor(5.9));
+// console.log(Math.min(5,6,7,8,12));
+// console.log(Math.max(5,6,7,8,12));
+
+
+// RANDOM
+//console.log(Math.random());
+//console.log(Math.floor(Math.random()*10) + 1);
+
+const min=1;
+const max=6;
+
+console.log(Math.floor(Math.random()*(max-min +1)) + min);
+
+
