6d1425751afc46064f73485c3294db3ff5c7d459
dikshapanwar <138571165+dikshapanwar@users.noreply.github.com>
2024-10-17 08:50:08 +0000

    FOR IN LOOP

---
+++ 05_Iteration/object.js
@@ -0,0 +1,34 @@
+const myObj ={
+    js:"javascript",
+    cpp:"c ++",
+    ruby:"ruby",
+    swift:"swift 1245"
+}
+
+for (const key in myObj) {
+    //console.log(myObj[key]);
+   //console.log(`${key} shortcut is for: ${myObj[key]}`);
+    
+}
+
+
+// ARRAY FOR IN
+
+const arr =[
+    "js","cpp","ruby","java"
+]
+
+for (const key in arr) {
+ // console.log(arr[key]);
+  
+}
+
+const map = new Map();
+ map.set("In","India")
+ map.set("USA","United States of America ")
+ map.set("Fr","France ")
+
+//  for (const key in map) {
+//     console.log(map[key]);
+    
+//  }
