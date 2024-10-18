953ea6096993f7ec73fba49a7acccfa0e7cc4018
dikshapanwar <138571165+dikshapanwar@users.noreply.github.com>
2024-10-18 09:52:09 +0000

    Switch Satetment

--- project/color/index.js
+++ project/color/index.js
@@ -1,20 +1,46 @@
-const buttons=document.querySelectorAll('.button')
-//console.log(buttons)
-const body=document.querySelector("body")
-buttons.forEach((item)=>{
-  // return console.log(item)
-  item.addEventListener('click',function(e){
-    if (e.target.id ==="grey") {
-    body.style.backgroundColor=e.target.id
+// const buttons=document.querySelectorAll('.button')
+// //console.log(buttons)
+// const body=document.querySelector("body")
+// buttons.forEach((item)=>{
+//   // return console.log(item)
+//   item.addEventListener('click',function(e){
+//     if (e.target.id ==="grey") {
+//     body.style.backgroundColor=e.target.id
+//     }
+//     if (e.target.id ==="white") {
+//       body.style.backgroundColor=e.target.id
+//       }
+//       if (e.target.id ==="blue") {
+//         body.style.backgroundColor=e.target.id
+//         }
+//         if (e.target.id ==="yellow") {
+//           body.style.backgroundColor=e.target.id
+//           }
+//   })
+// })
+
+
+//Switch Statement
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach((item) => {
+  item.addEventListener('click', function(e) {
+    switch (e.target.id) {
+      case "grey":
+        body.style.backgroundColor = "grey";
+        break;
+      case "white":
+        body.style.backgroundColor = "white";
+        break;
+      case "blue":
+        body.style.backgroundColor = "blue";
+        break;
+      case "yellow":
+        body.style.backgroundColor = "yellow";
+        break;
+      default:
+        break;
     }
-    if (e.target.id ==="white") {
-      body.style.backgroundColor=e.target.id
-      }
-      if (e.target.id ==="blue") {
-        body.style.backgroundColor=e.target.id
-        }
-        if (e.target.id ==="yellow") {
-          body.style.backgroundColor=e.target.id
-          }
-  })
-})
+  });
+});
