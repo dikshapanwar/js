13f0b7d95e192f91f1878c954cc12923368cafdc
dikshapanwar <138571165+dikshapanwar@users.noreply.github.com>
2024-10-18 11:12:09 +0000

    game

---
+++ project/game.js
@@ -0,0 +1,41 @@
+const randomNumnber = Math.floor(Math.random() * 100 + 1);
+//console.log(RandomNumnber)
+const submitBtn = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+
+const userGusses = document.querySelector('.guesses');
+const userLastResult = document.querySelector('.lastResult');
+const lowOrHigh = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p=document.createElement('p');
+let prevGuesses=[];
+let numGuesses=1;
+let playGame=true;
+
+
+function validateGuess(guess){
+
+}
+
+function checkGuess(guess){
+
+}
+function dispalyGuess(guess)
+{
+
+}
+function displayMessage(message){
+
+}
+
+
+function newGame()
+{
+
+}
+
+function endGame()
+{
+  
+}
